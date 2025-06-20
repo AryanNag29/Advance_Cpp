a37c693acf1e23531d1d631fe906c92b9f82da69
AryanNag29 <231030332@juitsolan.in>
2025-06-21 03:48:48 +0530

    Const Keyword

--- Variables and datatypes/Const_keyword.cpp
+++ Variables and datatypes/Const_keyword.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
-const double PI{3.14};
+const double PI{3.14}; // const double
 
 int main(){
     unsigned int radius;
     cout<<"Enter radius of circle:"<<endl;
     cin>>radius;
-    cout<<"Area of a Circle is: "<<PI*radius;
+    cout<<"Area of a Circle is: "<<PI*radius*radius;
 }
+
+/*
+1: when we put to a variable before it's data type we cannot change it's value
+for the entire life of the program.
+
+2: const can be modified using pointers by accessing the address location of the 
+const variable but it's not a good practice to do so because it defeat the purpos 
+of const keyword
+*/
