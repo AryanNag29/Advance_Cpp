5cc9ef18f1f7736791970a26b194f5eef3e18c70
AryanNag29 <231030332@juitsolan.in>
2025-06-21 03:47:03 +0530

    Const Keyword

---
+++ Variables and datatypes/Const_keyword.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+const double PI{3.14};
+
+int main(){
+    unsigned int radius;
+    cout<<"Enter radius of circle:"<<endl;
+    cin>>radius;
+    cout<<"Area of a Circle is: "<<PI*radius;
+}
