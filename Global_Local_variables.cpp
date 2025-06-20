99350d18cfadb1736a5423a1819dd6ec94f0f126
AryanNag29 <231030332@juitsolan.in>
2025-06-21 03:14:58 +0530

    moved to folder

--- Global_Local_variables.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-int a=10; // global variable are variable which is define outside of all function and classes
-
-int main(){
-    int b = 10; // local variable are variable which is define inside a function or class
-
-    std::cout<<a<<"\n"<<b;
-}
