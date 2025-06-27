ee3427b13edf781d165e9f2417881da6e058bbfa
AryanNag29 <231030332@juitsolan.in>
2025-06-28 01:37:51 +0530

    suffix int litrals

---
+++ Variables and datatypes/int_suffixes_litrals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 124;
+    unsigned int b = 124u;
+    long long c = 124l;
+    long long int d = 124ll;
+    return a;
+    return b;0
+    return c;
+    return d;
+
+    return 0;
+}
