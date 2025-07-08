e65d408853ee78bd6397d0995c341b33eee96be4
AryanNag29 <231030332@juitsolan.in>
2025-07-09 00:48:28 +0530

    Getline function of string

---
+++ Input And Output/getline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name{};
+    cout<<"Enter your name : ";
+    getline(cin,name); // getline function is use to cin strings in cpp;
+    cout<<"Welcome "<<name<<endl;
+}
