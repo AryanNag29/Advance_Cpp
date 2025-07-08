a3df2316c961d46d775592cbae9496c975040162
AryanNag29 <231030332@juitsolan.in>
2025-07-09 00:42:35 +0530

    Input_cpp

---
+++ Input And Output/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout<<"Enter two integer to be added"<<endl;
+    cin>>x>>y;
+    cout<<"Sum is: "<<x+y<<endl;
+
+
+    
+}
